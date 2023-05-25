2fa5cd8f84d6b1c53387effccf958367c1dc9cef
PARKPARKWOO <wy9295@naver.com>
2023-05-25 21:45:53 +0900

    Fix2

--- src/main/java/com/baeker/baeker/base/initDB/CreateAdmin.java
+++ src/main/java/com/baeker/baeker/base/initDB/CreateAdmin.java
@@ -42,8 +42,6 @@ public class CreateAdmin {
                 createMember("admin", "운영자", "", "https://avatars.dicebear.com/api/avataaars/600.svg", "sunnight9507");
             }
         }
-
-
         private Member createMember(String username, String nickName, String about, String img, String baekJoonName) throws IOException, ParseException {
             MemberJoinForm form = new MemberJoinForm(username, nickName, about, "1234", "1234", img);
             Member member = memberService.join(form).getData();
