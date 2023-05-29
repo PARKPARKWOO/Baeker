60d918989783db8f8ae6ca26bb24001e7bbb134a
박우영 <118690612+PARKPARKWOO@users.noreply.github.com>
2023-05-29 16:20:03 +0900

    Park (#18)
    * Fix: admin (#117)
    
    * Feat: prometheus
    
    * Fix
    
    * Fix2
    
    * Fix3
    
    * Fix3
    
    * 시큐리티 수정
    
    * yml 수정
    
    ---------
    
    Co-authored-by: I'm back <115536240+choideakook@users.noreply.github.com>

--- src/main/java/com/baeker/baeker/base/initDB/CreateAdmin.java
+++ src/main/java/com/baeker/baeker/base/initDB/CreateAdmin.java
@@ -24,7 +24,7 @@ public class CreateAdmin {
 
     @PostConstruct
     public void init() throws IOException, ParseException {
-        initService.createAdmin();
+        initService.init_alpha_and_study();
     }
 
     @Component
@@ -36,14 +36,14 @@ public class CreateAdmin {
         private final SolvedApiService solvedApiService;
 
 
-        public void createAdmin() throws IOException, ParseException {
+
+        public void init_alpha_and_study() throws IOException, ParseException {
+
             Optional<Member> member = memberService.getMember("admin");
             if (member.isEmpty()) {
                 createMember("admin", "운영자", "", "https://avatars.dicebear.com/api/avataaars/600.svg", "sunnight9507");
             }
         }
-
-
         private Member createMember(String username, String nickName, String about, String img, String baekJoonName) throws IOException, ParseException {
             MemberJoinForm form = new MemberJoinForm(username, nickName, about, "1234", "1234", img);
             Member member = memberService.join(form).getData();
